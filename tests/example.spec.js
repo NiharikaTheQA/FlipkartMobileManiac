e71dff1115b74f439696ee542e1d2b49ed851b77
Niharika <dubeyniharika95@gmail.com>
2023-03-02 11:38:00 +0530

    FeedBacks Implemented

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,18 @@
 import { test, expect, Page } from '@playwright/test';
-const {HomePage} = require("../Pages/HomePage");
 const testData = require("../Resources/testData.json");
 
 test.describe('Demo Test', () => {
-
   test('FLIPKART LISTING', async ({ page }) => {
-  const homePage = new HomePage(page);
 
   await page.goto(testData.baseURL);
-  await homePage.closeLoginButton.click();
-  await homePage.nextButton.hover();
-  await homePage.banner.click();
-  await homePage.electronicPanel.click();
-  await expect(homePage.mobilePhoneList).toHaveCount(18);
-  const allText = await homePage.mobilePhoneList.allTextContents();
+  await page.getByRole('button', { name: '✕' }).click();
+  await page.locator('._2t2dSp').hover();
+  const bannerImage= page.locator("//body//div[@id='container']//div//div//div//div[2]//div[1]//a[1]//div[1]//img[2]");
+  await bannerImage.dispatchEvent('click');
+  await page.locator('span').filter({ hasText: 'Electronics' },{ exact: true }).click();
+  const mobileBrands = page.locator("//*[contains(@title,'Mobiles')]//following-sibling::a");
+  await expect(mobileBrands).toHaveCount(18);
+  const allText = await mobileBrands.allTextContents();
   console.log("\n\nList of all the mobile brands present in flipkart are as listed below:\n")
   for (let i = 0; i < (allText.length)-1; i++)
   {    
